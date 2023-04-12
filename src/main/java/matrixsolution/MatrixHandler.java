e8df48105cbf564725f3916209f13d0cde79953e
bendichsm22pv191 <serg.bendich@gmail.com>
2023-04-12 14:37:02 +0300

    reduced the number of steps in functions by creating new functions

--- src/main/java/matrixsolution/MatrixHandler.java
+++ src/main/java/matrixsolution/MatrixHandler.java
@@ -27,37 +27,51 @@ public class MatrixHandler {
         System.out.println("Write matrix with size " + rowsCount + " x " + columnsCount);
 
         for (int i = 0; i < rowsCount; ++i) {
-            for (int j = 0; j < columnsCount; ++j) {
-                this.matrix[i][j] = scanner.nextInt();
-            }
+            inputArray(this.matrix[i], this.columnsCount, scanner);
         }
     }
+
+    public void inputArray(int[] array, int lenArray, Scanner scanner){
+        for (int elNumber = 0; elNumber < lenArray; ++elNumber) {
+            array[elNumber] = scanner.nextInt();
+        }
+    }
+
 
     public void outputMatrix() {
         System.out.println("Our matrix with size " + this.rowsCount + " x " + this.columnsCount);
         int intermediateOutputDistance = 3;
-        int lenOutputMatrixSymbol = maxLenMatrixSymbol(this.matrix) + intermediateOutputDistance;
+        int lenOutputMatrixSymbol = findMaxLenMatrixElement() + intermediateOutputDistance;
         String format = "%" + lenOutputMatrixSymbol + "d ";
-        for (int i = 0; i < this.rowsCount; ++i) {
-            for (int j = 0; j < this.columnsCount; ++j) {
-                System.out.printf(format, this.matrix[i][j]);
-            }
+        for (int rowNumber = 0; rowNumber < this.rowsCount; ++rowNumber) {
+            outputArrayWithFormat(this.matrix[rowNumber], format);
             System.out.println("\n");
         }
     }
 
-    public int maxLenMatrixSymbol(int[][] matrix) {
-        int len;
+    public void outputArrayWithFormat(int[] array, String format){
+        for (int elNumber = 0; elNumber < this.columnsCount; ++elNumber) {
+            System.out.printf(format, array[elNumber]);
+        }
+    }
+
+    public int findMaxLenMatrixElement() {
         int maxLen = 0;
-        for (int i = 0; i < this.rowsCount; ++i) {
-            for (int j = 0; j < this.columnsCount; ++j) {
-                len = ("" + matrix[i][j]).length();
-                if (len > maxLen) {
-                    maxLen = len;
-                }
-            }
+        for (int rowNumber = 0; rowNumber < this.rowsCount; ++rowNumber) {
+            maxLen = findArrayElementMoreThenQuantity(this.matrix[rowNumber], this.columnsCount, maxLen);
         }
         return maxLen;
+    }
+
+    public int findArrayElementMoreThenQuantity(int[] array, int lenArray, int quantity){
+        int len;
+        for (int elNumber = 0; elNumber < lenArray; ++elNumber) {
+            len = ("" + array[elNumber]).length();
+            if (len > quantity) {
+                quantity = len;
+            }
+        }
+        return quantity;
     }
 
     public MatrixHandler multiplyMatrix(MatrixHandler matrix) {
@@ -69,17 +83,31 @@ public class MatrixHandler {
             int[][] outMatrix = new int[this.rowsCount][resultColumnsCount];
             resultMatrix = new MatrixHandler(outMatrix, this.rowsCount, resultColumnsCount);
 
-            for (int i = 0; i < this.rowsCount; ++i) {
-                for (int j = 0; j < resultColumnsCount; ++j) {
-                    for (int k = 0; k < this.columnsCount; ++k) {
-                        outMatrix[i][j] += this.matrix[i][k] * multiplierMatrix[k][j];
-                    }
-                }
+            for (int rowNumber = 0; rowNumber < this.rowsCount; ++rowNumber) {
+                outMatrix[rowNumber] =
+                        multiplyArrayByMatrixColumns(this.matrix[rowNumber], this.columnsCount, multiplierMatrix, resultColumnsCount);
             }
             resultMatrix.setMatrix(outMatrix);
         } else {
             System.err.println("Different values of columns and rows in matrices when multiplying");
         }
         return resultMatrix;
+    }
+
+    public int[] multiplyArrayByMatrixColumns(int[] array, int lenArray, int[][] matrix, int columnsCount){
+        int[] resultArray = new int[columnsCount];
+        for (int columnNumber = 0; columnNumber < columnsCount; ++columnNumber) {
+            resultArray[columnNumber] = multiplyArrayByMatrixColumn(array, lenArray, matrix, columnNumber);
+        }
+        return resultArray;
+    }
+
+
+    public int multiplyArrayByMatrixColumn(int[] array, int lenArray, int[][] matrix, int columnNumber){
+        int resultElement = 0;
+        for (int elNumber = 0; elNumber < lenArray; ++elNumber) {
+            resultElement += array[elNumber] * matrix[elNumber][columnNumber];
+        }
+        return resultElement;
     }
 }
